f7ee8b00a7651c0f393f7c23fa5c89ef381fd11d
tanvirulislam912 <mdtanvir1109040@gmail.com>
2024-07-15 16:52:50 +0600

    first commit

---
+++ js/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TourPlan from "./components/TourPlan/TourPlan";
+
+function App() {
+    return (
+        <div>
+            <TourPlan></TourPlan>
+        </div>
+    );
+}
+
+export default App;
